e32f88eb3b69ed070918b25d03f04748ac9eb2d2
dsuccar <danielsuccar@icloud.com>
2020-04-06 16:57:46 -0400

    Done.

---
+++ db/migrate/20200406193516_add_column.rb
@@ -0,0 +1,6 @@
+class AddColumn < ActiveRecord::Migration[5.0]
+  def change
+    add_column :songs, :artist_id, :integer
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+dan = Artist.create(name: 'Danny')
+ulb = Song.create(title: 'Ultra Light Beam', artist_id: dan)
